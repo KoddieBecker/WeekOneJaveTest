dd4c6a82ea22f2e04fba74241a052ed369e98361
Koddie Becker <koddiebecker@Koddies-MacBook-Pro-5.local>
2018-01-12 15:20:34 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import jdk.nashorn.api.tree.WhileLoopTree;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //  Boolean ask = true;
+        Scanner sc = new Scanner(System.in);
+
+        //  while (ask) {
+        System.out.println("Hello!");
+
+        //     String[] questions = {
+        //              "Please enter your name",
+        //              "Please enter your age",
+        //             "Please enter the current year",
+        //    };
+
+        //      List<String> answers = new ArrayList<>();
+
+        //      for (int i = 0; i < questions.length; i++) {
+        //                   System.out.println(questions[i]);
+        //                     String userEntry = sc.nextLine();
+        //}                    answers.add();
+    }
+}
+     
